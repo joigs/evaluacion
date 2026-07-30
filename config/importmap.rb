cdb45d93b0df558671d43a1bd40d8bb6e4b0e7c5
joigs <jose.saez2001@alumnos.ubiobio.cl>
2026-07-30 12:23:01 -0400

    cambio

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,6 +12,7 @@ pin_all_from "app/javascript/custom", under: "custom"
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "flowbite-datepicker" # @1.3.0
-pin "sweetalert2" # @11.14.0
 pin "flatpickr", to: "https://cdn.jsdelivr.net/npm/flatpickr"
 pin "flatpickr/es", to: "https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/es.js"
+pin "sweetalert2",
+    to: "sweetalert2-neutral.js" # @11.22.2-neutral
